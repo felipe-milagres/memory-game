15feb4989ac8fcc31cc6d7f5d8f3f4e2a96e47b0
Felipe Milagres <felipemilagres.x@gmail.com>
2014-11-03 14:23:48 -0200

    first commit

---
+++ Assets/scripts/Card.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Card : MonoBehaviour {
+
+	// card states
+	public enum state { NOT_FLIPPED , FLIPPED , MATCHED };
+	public state myCardState;
+	public string myCardName;
+
+	private void Start () {
+		// inicial state
+		myCardState = state.NOT_FLIPPED;
+		myCardName = gameObject.transform.name;
+
+		//Debug.Log (myCardName);
+	}
+
+	public void FlipCard() {
+
+		//if( myCardState == state.NOT_FLIPPED ){
+			myCardState = state.FLIPPED;
+			StartCoroutine( RotateMe(Vector3.up * 180, 0.8f) );
+		//}
+
+	}
+
+	public void FlipBackCard() {
+
+		Debug.Log (myCardName + ":" + myCardState);
+
+		//if( myCardState == state.FLIPPED ){
+			myCardState = state.NOT_FLIPPED;
+			StartCoroutine( RotateMe(Vector3.down * 180, 1) );
+		//}
+
+		Debug.Log (myCardName + ":" + myCardState);
+
+	}
+
+	public void CardMatch() {
+		Debug.Log( "function CardMatch" );
+		myCardState = state.MATCHED;
+	}
+
+	private IEnumerator RotateMe( Vector3 byAngles, float inTime ) {
+
+		Quaternion fromAngle = transform.rotation;
+		Quaternion toAngle = Quaternion.Euler(transform.eulerAngles + byAngles);
+
+		for( float t = 0f; t < 1; t += Time.deltaTime/inTime ) {
+
+			transform.rotation = Quaternion.Lerp(fromAngle, toAngle, t);
+			yield return null;
+
+		}
+
+		// avoid 181 or 180.99 degrees
+		transform.rotation = toAngle;
+
+	}
+
+}
---
+++ Assets/scripts/MatchCards.cs
@@ -0,0 +1,135 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class MatchCards : MonoBehaviour {
+	
+	//List<GameObject> cardsClicked = new List<GameObject>();
+	Queue cardsClickedQueue = new Queue();
+
+	private void Start() { //Debug.Log( cardsClicked.Count );
+	}
+
+	private void Update() {
+		// mouse click event
+		if (Input.GetMouseButtonDown(0)) {
+			CastRay();
+		}
+	}
+	
+	private void CastRay() {
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		RaycastHit hit;
+		
+		// if raycast hits in something
+		if( Physics.Raycast( ray, out hit, 100 ) ){
+
+			if( cardsClickedQueue.Count < 2 ){
+				// if raycast hits in my unflipped card
+				if( hit.collider.gameObject.tag == "card" && hit.transform.GetComponent<Card>().myCardState == Card.state.NOT_FLIPPED ){
+
+
+					// flipping my card
+					hit.transform.SendMessage("FlipCard");
+
+					// caching my card in a list
+					//cardsClicked.Add( hit.transform.gameObject );
+					cardsClickedQueue.Enqueue( hit.transform.gameObject );
+
+				}
+
+
+			}
+
+			// two cards in my list
+			if( cardsClickedQueue.Count == 2 ){
+				
+				GameObject temp1 = (GameObject) cardsClickedQueue.Dequeue();
+				GameObject temp2 = (GameObject) cardsClickedQueue.Dequeue();
+
+				if( Match ( temp1 , temp2 ) ){
+					Debug.Log( "Match");
+				}
+				else {
+					Debug.Log( "NOT Match");
+					StartCoroutine ( Example( temp1 , temp2 ) );
+				}
+				//Debug.Log("--> " +  cardsClicked[0] + " " + cardsClicked[1]);
+				/*
+				if( Match() ){
+					// changing the cards status FLIPPED to MATCHED
+					//cardsClicked[0].transform.SendMessage("CardMatch");
+					//cardsClicked[1].transform.SendMessage("CardMatch");
+					Debug.Log( "Match");
+				}
+				else { 
+					// virar todas as cartas que estao com o estado de FLIPPED para NOT_FLIPPED. nao virar os MATCHED
+
+					//Debug.Log( "sad");
+					//StartCoroutine ( Example( cardsClicked ) );
+					//Debug.Log( "dsa");
+				}
+*/
+				// cleaning cards in cache
+				//cardsClicked.Clear();
+				//Debug.Log( ">>>>>> cardsClicked.Clear" );
+			}
+		}
+	}
+	/*
+	void RotteMe() {
+		//Debug.Log( "A" );
+		// I have to wait my cards flip complety first ----> colocar uma verificacao de se a carta esta virando , enquanto estiver virando o FlipBackCard nao e executado 
+		//yield return new WaitForSeconds(1); 
+		//Debug.Log( "B" );
+		cardsClicked[0].transform.SendMessage("FlipBackCard");
+		cardsClicked[1].transform.SendMessage("FlipBackCard");	
+		//Debug.Log( "C" );
+	}
+
+	private bool Match() {
+		// if my cards have the same Letter 
+		// "card_A1" => slipt('_') => "A1" => Remove(1) => "A" => ToCharArray() => 'A'
+		/*
+		if( cardsClicked[0].GetComponent<Card>().myCardName.Split('_')[1].Remove(1).ToCharArray()[0] == 
+		    cardsClicked[1].GetComponent<Card>().myCardName.Split('_')[1].Remove(1).ToCharArray()[0] ) 
+			return true;
+		else 
+			return false; 
+	}*/
+			
+	private bool Match( GameObject gm1 , GameObject gm2 ) {
+		// if my cards have the same Letter 
+		// "card_A1" => slipt('_') => "A1" => Remove(1) => "A" => ToCharArray() => 'A'
+
+		if( gm1.GetComponent<Card>().myCardName.Split('_')[1].Remove(1).ToCharArray()[0] == 
+		   gm2.GetComponent<Card>().myCardName.Split('_')[1].Remove(1).ToCharArray()[0] ) 
+				return true;
+			else 
+				return false; 
+	}
+
+	IEnumerator Example( GameObject gm1 , GameObject gm2 ) {
+
+		Debug.Log(Time.time);
+		yield return new WaitForSeconds(5);
+		gm1.transform.SendMessage("FlipBackCard");
+		gm2.transform.SendMessage("FlipBackCard");
+		Debug.Log(Time.time);
+
+	}
+
+	IEnumerator Example( List<GameObject> list ) {
+
+		List<GameObject> cardsClicked1 = list;
+
+		Debug.Log(Time.time);
+		yield return new WaitForSeconds(5);
+		cardsClicked1[0].transform.SendMessage("FlipBackCard");
+		cardsClicked1[1].transform.SendMessage("FlipBackCard");
+		Debug.Log(Time.time);
+
+		cardsClicked1.Clear ();
+	}
+
+}
