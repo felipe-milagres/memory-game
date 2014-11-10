3929edf4f2f71b6b77d61937d5a73c52932ae469
Felipe Milagres <felipemilagres.x@gmail.com>
2014-11-10 19:00:50 -0200

    xml database update

---
+++ Assets/useless/scripts/MainXML.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainXML : MonoBehaviour{
+
+	// Use this for initialization
+	void Start () {
+		ParseXML pXML = new ParseXML( "data" );
+		
+		//Debug.Log( pXML.getElementXML( "best_time" ) );
+		//Debug.Log( minutesToSeconds( "15:00" ) );
+
+		pXML.ReadXML ();
+		pXML.changeValueOf ("best_time", "25:00");
+		pXML.ReadXML();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	// int timeInSeconds = minutesToSeconds( "15:00" );
+	private int minutesToSeconds( string time ){
+		// split MM:SS to MM and SS
+		int minutes = int.Parse( time.Split(':')[0] );
+		int seconds = int.Parse( time.Split(':')[1] );
+		return ( minutes * 60 ) + seconds;
+	}
+}
---
+++ Assets/useless/scripts/ParseXML.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Xml;
+using UnityEditor;
+
+public class ParseXML {
+	
+	private XmlDocument _xml;
+	private TextAsset _textXML;
+
+	public ParseXML( string xmlName ){
+		_xml = new XmlDocument();
+		_textXML = (TextAsset) Resources.Load( xmlName , typeof(TextAsset) );
+		LoadXML( xmlName );
+	}
+
+	private void LoadXML( string xmlName ){
+		_xml.LoadXml( _textXML.text );
+		Debug.Log( "XML LOADED" );
+	}
+
+	private void saveChanges(){
+		_xml.Save( AssetDatabase.GetAssetPath( _textXML ) );
+		Debug.Log( "CHANGES SAVED" );
+	}
+
+	public void ReadXML(){
+		XmlNode root = _xml.FirstChild;
+		foreach( XmlNode node in root.ChildNodes ){
+			Debug.Log( "<" + node.LocalName + ">" );
+			foreach( XmlNode sub_node in node.ChildNodes ){
+				//Debug.Log( "NodoName=" + node.LocalName + " | SubNodoName=" + sub_node.LocalName + " | NodoContent=" + sub_node.InnerText );
+				Debug.Log( "   <" + sub_node.LocalName + ">" + sub_node.InnerText + "<" + sub_node.LocalName + ">" );
+			}
+		}
+	}
+
+	public string getElementXML( string elementName ){
+		string retorno = null;
+		XmlNode root = _xml.FirstChild;
+		foreach( XmlNode node in root.ChildNodes ){
+			foreach( XmlNode sub_node in node.ChildNodes ){
+				if( sub_node.LocalName == elementName )
+					retorno = sub_node.InnerText;
+			}
+		}
+		Debug.Log( "ELEMENT: " + retorno );
+		return retorno;
+	}
+
+	public void changeValueOf( string elementName , string value ){
+		XmlNode root = _xml.FirstChild;
+		foreach( XmlNode node in root.ChildNodes ){
+			foreach( XmlNode sub_node in node.ChildNodes ){
+				if( sub_node.LocalName == elementName ){
+					sub_node.InnerText = value;
+					break;
+				}
+			}
+		}
+		saveChanges();
+	}
+
+	private void resetXML(){
+		XmlNode root = _xml.FirstChild;
+		foreach( XmlNode node in root.ChildNodes ){
+			foreach( XmlNode sub_node in node.ChildNodes ){
+				if( sub_node.LocalName == "best_time" ){ sub_node.InnerText = "99:99"; }
+			}
+		}
+	}
+
+}
